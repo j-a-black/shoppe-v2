2f8c1023f2db9e2bc32de35398a3754a1fcbb46b
jasonanthonblack@gmail.com <jasonb168@gmail.com>
2021-08-13 19:22:40 -0500

    Add To Cart button pushes products to array

--- dist/js/products.js
+++ dist/js/products.js
@@ -4,7 +4,7 @@ export const render = (data) => {
   data.forEach((data) => {
     let html = "";
     html = `
-      <article class="card">
+      <article id="${data.id}" class="card">
         <div class="card__figure">
           <img
             src="${data.img}"
@@ -13,8 +13,8 @@ export const render = (data) => {
         </div>
         <div class="card__body">
           <span class="card__title">${data.title}</span>
-          <span>$${data.price}</span>
-          <button class="btn">Add To Cart</button>
+          <span class="card__price-js">$${data.price}</span>
+          <button class="btn add-to-cart-js">Add To Cart</button>
         </div>
       </article>
   `;
@@ -22,7 +22,26 @@ export const render = (data) => {
   });
 };
 
+let cartArr = [];
 export const handleClick = (e) => {
-  if (e.target.className === "btn") console.log("its a button");
-  //
+  if (e.target.className === "btn add-to-cart-js") {
+    let button = e.target;
+    let product = button.parentElement.parentElement;
+    let productId = product.getAttribute("id");
+    let productImageSrc = product.querySelector("img").src;
+    let productName = product.querySelector(".card__title").innerText;
+    let productPrice = product
+      .querySelector(".card__price-js")
+      .innerText.slice(1);
+    let productObj = { productId, productImageSrc, productName, productPrice };
+    if (cartArr.length < 1) {
+      cartArr.push(productObj);
+    }
+    let foundItem = cartArr.some(
+      (item) => item.productId === productObj.productId
+    );
+    if (!foundItem) cartArr.push(productObj);
+
+    console.log(cartArr);
+  }
 };
