e81d975292354c199cb717b31da5c59adeaeefe7
jasonanthonblack@gmail.com <jasonb168@gmail.com>
2021-08-15 15:06:57 -0500

    disable scrolling when mobile nav menu is active

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -3,6 +3,7 @@ export const toggleMobileNavBtnAndMenu = () => {
   const mobileMenu = document.querySelector(".mobile-nav-menu");
   const mobileLinks = document.querySelectorAll(".mobile-link-js");
   const body = document.body;
+  console.log(body);
 
   // Changes from hamburger to 'X'
   hamburger.classList.toggle("fa-bars");
@@ -10,17 +11,31 @@ export const toggleMobileNavBtnAndMenu = () => {
 
   // Toggles mobile nav menu and prevents scolling
   mobileMenu.classList.toggle("is-active");
-  mobileMenu.classList.contains("is-active")
-    ? (body.style.position = "fixed")
-    : (body.style.position = "relative");
+  if (mobileMenu.classList.contains("is-active")) {
+    body.classList.add("no-scrolling");
+    html.classList.add("no-scrolling");
+  } else {
+    body.classList.remove("no-scrolling");
+    html.classList.remove("no-scrolling");
+  }
+
+  // mobileMenu.classList.contains("is-active")
+  //   ? (body.style.position = "fixed")
+  //   : (body.style.position = "relative");
 
   // Closes mobile menu when link is clicked
   mobileLinks.forEach((link) => {
     link.addEventListener("click", () => {
       mobileMenu.classList.remove("is-active");
 
-      // Allows scrolling once menue is closed
-      if (body.style.position === "fixed") body.style.position = "relative";
+      // Allows scrolling once menu is closed
+      if (
+        body.classList.contains("no-scrolling") &&
+        html.classList.contains("no-scrolling")
+      ) {
+        body.classList.remove("no-scrolling");
+        html.classList.remove("no-scrolling");
+      }
 
       // Shows hamburger icon instead of 'X' icon
       hamburger.classList.add("fa-bars");
